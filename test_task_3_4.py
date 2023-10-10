9e1994de17571101b72c86bbde5c04dcb3c0f17a
Funt <funt_88@mail.ru>
2023-10-10 14:42:46 +0300

    homework 2 finished

---
+++ test_task_3_4.py
@@ -0,0 +1,24 @@
+import pytest
+from _3_4 import checkout_text, getout
+
+folder_out = "/home/funt/folder_out/"
+folder_ext = "/home/funt/folder_ext/"
+dz_2 = "/home/funt//Autotest_Linux_console_applications/"
+
+# Показать файлы в папке не запуская
+def test_7z_l():
+    assert checkout_text(f'cd {folder_out}; 7z l ./archiv.7z', "Listing archive: ./archiv.7z"), "test_l FAIL"
+
+
+# Разархивировать файлы из папки
+def test_7z_x():
+    assert checkout_text(f'cd {folder_ext}; 7z x {folder_out}archiv.7z', "Everything is Ok"), "test_x FAIL"
+
+# Проверка хэша
+def test_7z_h():
+    crc32_hash = getout(f'cd {dz_2}; crc32 checkers.py').upper()
+    assert checkout_text(f'cd {dz_2}; 7z h checkers.py', crc32_hash), "test_h FAIL"
+
+
+if __name__ == '__main__':
+    pytest.main()
