83b14521b6722f1d9dec55ad618e39ef7c590b92
Funt <funt_88@mail.ru>
2023-10-17 00:17:32 +0300

    Paramiko, деплой, SSH

--- Seminar_SSH/deploy.py
+++ Seminar_SSH/deploy.py
@@ -2,10 +2,10 @@ from sshcheckers import ssh_checkout, upload_files
 
 def deploy():
     res = []
-    upload_files("0.0.0.0", "funt", "11", "tests/p7zip-full.deb", "/home/funt/p7zip-full.deb")
-    res.append(ssh_checkout("0.0.0.0", "funt", "11", "echo '11' | sudo -S dpkg -i /home/funt/p7zip-full.deb",
+    upload_files("0.0.0.0", "user2", "11", "tests/p7zip-full.deb", "/home/user2/p7zip-full.deb")
+    res.append(ssh_checkout("0.0.0.0", "user2", "11", "echo '11' | sudo -S dpkg -i /home/user2/p7zip-full.deb",
                             "Настраивается пакет"))
-    res.append(ssh_checkout("0.0.0.0", "funt", "11", "echo '11' | sudo -S dpkg -s p7zip-full",
+    res.append(ssh_checkout("0.0.0.0", "user2", "11", "echo '11' | sudo -S dpkg -s p7zip-full",
                             "Status: install ok installed"))
     return all(res)
 
